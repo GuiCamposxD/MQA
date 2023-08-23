22857b73161812b0228e64b90e830c651496e7d4
GuiCampos <gui.xglpx@gmail.com>
2023-08-23 18:29:59 -0300

    fix-up

--- main.py
+++ main.py
@@ -7,12 +7,10 @@ path = os.path.join(pre, fname)
 
 adult = pd.read_excel(path, na_values='?')
 
-adult_filled = adult.fillna(adult.mode())
+adult['Ocupação'].fillna(adult['Ocupação'].mode()[0], inplace=True)
+adult['País de Origem'].fillna(adult['País de Origem'].mode()[0], inplace=True)
 
-adult_filled['Classe de Trabalho'].fillna(adult_filled['Classe de Trabalho'].mode()[0], inplace=True)
-adult_filled['País Nativo'].fillna(adult_filled['País Nativo'].mode()[0], inplace=True)
-
-output_path = 'adult-data-alterado.xlsx'
-adult_filled.to_excel(output_path, index = False)
+output_path = 'adult_data_alterado.xlsx'
+adult.to_excel(output_path, index = False)
 
 print(output_path)
