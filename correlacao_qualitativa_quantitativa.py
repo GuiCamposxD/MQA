c2d48781ea177cdfd7157f3becc0d18a90ca50b6
GuiCampos <gui.xglpx@gmail.com>
2023-08-25 17:32:40 -0300

    add qualitative and quantitative correlation and multicollinearity

---
+++ correlacao_qualitativa_quantitativa.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from scipy.stats import pointbiserialr
+import matplotlib.pyplot as plt
+import os
+
+pre = os.path.dirname(os.path.realpath(__file__))
+fname = 'adult.xlsx'
+path = os.path.join(pre, fname)
+
+adult = pd.read_excel(path)
+
+adult['Renda'] = adult['Renda'].map({'<=50K': 0, '>50K': 1})
+
+variaveis_quantitativas = ['Idade', 'Anos de Estudo', 'Ganho de Capital', 'Perca de Capital', 'Carga Horária Semanal']
+
+resultado_correlacao = []
+
+for var in variaveis_quantitativas:
+    correlacao = pointbiserialr(adult[var], adult['Renda'])
+    resultado_correlacao.append((var, correlacao.correlation, correlacao.pvalue))
+
+variaveis = [resultado[0] for resultado in resultado_correlacao]
+correlacoes = [resultado[1] for resultado in resultado_correlacao]
+
+plt.bar(variaveis, correlacoes)
+plt.xlabel('Variáveis Quantitativas')
+plt.ylabel('Correlação Point-Biserial')
+plt.title('Correlação entre Variáveis Quantitativas e Renda')
+plt.xticks(rotation=45)
+plt.tight_layout()
+
+for i, corr in enumerate(correlacoes):
+    plt.text(i, corr, f'{corr:.2f}', ha='center', va='bottom')
+
+plt.show()
---
+++ multicolinearidade.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+from statsmodels.tools.tools import add_constant
+import os
+
+pre = os.path.dirname(os.path.realpath(__file__))
+fname = 'adult.xlsx'
+path = os.path.join(pre, fname)
+
+adult = pd.read_excel(path)
+
+variaveis_quantitativas = ['Idade', 'Anos de Estudo', 'Ganho de Capital', 'Perca de Capital', 'Carga Horária Semanal']
+
+adult_recortado = adult[variaveis_quantitativas]
+adult_recortado_com_constante = add_constant(adult_recortado)
+
+vif_data = pd.DataFrame()
+vif_data["feature"] = adult_recortado_com_constante.columns
+vif_data["VIF"] = [
+    variance_inflation_factor(adult_recortado_com_constante.values, i) for i in range(adult_recortado_com_constante.shape[1])
+]
+
+print(vif_data)
+
+plt.figure(figsize=(10, 6))
+bars = plt.barh(vif_data["feature"], vif_data["VIF"], color='skyblue')
+plt.xlabel('VIF Value')
+plt.ylabel('Variáveis')
+plt.title('Valores do VIF')
+plt.gca().invert_yaxis()
+for bar in bars:
+    plt.text(bar.get_width(), bar.get_y() + bar.get_height()/2, f'{bar.get_width():.2f}', 
+             va='center', ha='left', color='black', fontsize=10)
+
+plt.show()
