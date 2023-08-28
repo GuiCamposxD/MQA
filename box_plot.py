0641e5ed7270fd652e54a81b6ce9305ca8447261
GuiCampos <gui.xglpx@gmail.com>
2023-08-27 21:25:22 -0300

    fix-up

--- box_plot.py
+++ box_plot.py
@@ -14,17 +14,28 @@ fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(18, 6))
 variaveis_quantitativas = ['Idade', 'Anos de Estudo', 'Carga Horária Semanal']
 
 for i, variavel in enumerate(variaveis_quantitativas):
+    sns.violinplot(
+        data=adult, 
+        x='Sexo',
+        y=variavel,
+        palette='turbo',
+        inner=None,
+        linewidth=0,
+        saturation=0.4,
+        ax=axes[i],
+    )
     sns.boxplot(
         x='Sexo',
         y=variavel,
         data=adult,
-        ax=axes[i],
-        palette='Set3',
+        palette='turbo',
+        width=0.3,
+        boxprops={'zorder': 2},
         medianprops={'color': 'red'},
+        ax=axes[i]
     )
     axes[i].set_xlabel('Sexo')
     axes[i].set_ylabel(variavel)
     axes[i].set_title(variavel + ' por Sexo')
 
-plt.tight_layout()
 plt.show()
