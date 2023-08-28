1f694305ad669373acb102ef569a62dd051ee99c
Marcelo de Menezes Nascimento <mar_chelo10@hotmail.com>
2023-08-27 21:16:25 -0300

    atualizações de erros

--- numero_ocorrencias.py
+++ numero_ocorrencias.py
@@ -4,7 +4,7 @@ import os
 file_path = 'adult.xlsx'
 data = pd.read_excel(file_path)
 
-contagem_valor = data['Raça'].value_counts()
+contagem_valor = data['Etnia'].value_counts()
 print(contagem_valor, '\n')
 
 contagem_valor = data['Sexo'].value_counts()
