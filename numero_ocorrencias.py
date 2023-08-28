f979974eebc7902b40f805422843afe795503717
Marcelo de Menezes Nascimento <mar_chelo10@hotmail.com>
2023-08-27 21:12:47 -0300

    funções de medidas resumo

---
+++ numero_ocorrencias.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import os
+
+file_path = 'adult.xlsx'
+data = pd.read_excel(file_path)
+
+contagem_valor = data['Raça'].value_counts()
+print(contagem_valor, '\n')
+
+contagem_valor = data['Sexo'].value_counts()
+print(contagem_valor, '\n')
+
+contagem_valor = data['Renda'].value_counts()
+print(contagem_valor, '\n')
