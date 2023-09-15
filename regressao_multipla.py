7fdf8245b713b131e85f9a3434496f140e5f6e53
Marcelo de Menezes Nascimento <mar_chelo10@hotmail.com>
2023-09-15 16:57:26 -0300

    Regressão Múltipla

---
+++ regressao_multipla.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import statsmodels.api as sm
+import os
+import matplotlib.pyplot as plt
+
+#caminho do arquivo
+pre = os.path.dirname(os.path.realpath(__file__))
+fname = 'Gastos-Academia.xlsx'
+path = os.path.join(pre, fname)
+
+# Carregar os dados do Excel em um DataFrame do Pandas
+df = pd.read_excel(path)
+
+# Definir as variáveis independentes (X) e a variável dependente (Y)
+X = df[['horas', 'consumo']]  # variáveis independentes
+Y = df['gastos']  #  variável dependente
+
+# Adicionar uma constante para o termo de intercepto (β0)
+X = sm.add_constant(X)
+
+# Ajustar o modelo de regressão
+modelo = sm.OLS(Y, X).fit()
+
+#obtém os valores estimados
+Y_estimado = modelo.predict(X)
+
+
+# Imprimir o resumo estatístico do modelo
+print(modelo.summary())
+
+#plt.scatter(variavel eixo y, variável com valores reais ou valores estimados, cor, label da legenda)
+plt.scatter(df['periodo'], Y, color='black', label='Valores Reais')
+plt.scatter(df['periodo'], Y_estimado, color='red', label='Valores Estimados')
+
+plt.xlabel('Período')
+plt.ylabel('Gastos')
+plt.title('Valores Reais vs. Valores Estimados')
+plt.legend()
+plt.show()
+
+
+
+
+
